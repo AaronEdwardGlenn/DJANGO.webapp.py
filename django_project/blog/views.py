cf54217dcd38659cdd924835a478c70e97f1aecd
Aaron Edward Glenn <53454869+AaronEdwardGlenn@users.noreply.github.com>
2020-05-19 14:58:23 -0700

    creating dummy post data

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'Aaron Glenn',
+        'title': 'Blog 1',
+        'content': 'My First Blog',
+        'date_posted': 'January 1, 2020'
+    },
+    {
+        'author': 'Aaron Glenn',
+        'title': 'Blog 2',
+        'content': 'My Second Blog',
+        'date_posted': 'January 2, 2020'
+    }
+]
+
 
 def home(request):
     return render(request, 'blog/home.html')
