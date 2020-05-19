b3c8ea474d1caf3bb49ada9152fbd4ebf457e6cc
Aaron Edward Glenn <53454869+AaronEdwardGlenn@users.noreply.github.com>
2020-05-18 17:57:56 -0700

    adding template html files and configuring

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -3,11 +3,8 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    return HttpResponse('<h1>Blog Home All Day</h1>')
 
 
 def about(request):
     return HttpResponse('<h1>About</h1>')
-
-
-# Create your views here.
--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -30,7 +30,8 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+INSTALLEblog.apps.BlogConfig
+    'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
