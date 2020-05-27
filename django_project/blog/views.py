f9aaec9ff2a51aa07f09f2f8ec679511f328b04e
Aaron Edward Glenn <53454869+AaronEdwardGlenn@users.noreply.github.com>
2020-05-27 13:16:14 -0700

    created class based view with template for indiviual user posts

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 
@@ -11,6 +11,13 @@ def home(request):
 
 
 class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+class PostDetailView(ListView):
     model = Post
 
 
