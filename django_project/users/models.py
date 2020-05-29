542f8de765a146dc2d2bad473c98fbf3170b508b
Aaron Edward Glenn <53454869+AaronEdwardGlenn@users.noreply.github.com>
2020-05-28 20:37:11 -0700

    commenting out resizing saved user images function becuase it won't apply to S3 bucket on AWS

--- django_project/users/models.py
+++ django_project/users/models.py
@@ -10,12 +10,15 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'
 
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
 
-        img = Image.open(self.image.path)
+''' This following code is for image resizing on the filesystem but this wont apply when we use S3 storage '''
 
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
+# def save(self, *args, **kwargs):
+#     super().save(*args, **kwargs)
+#
+#     img = Image.open(self.image.path)
+#
+#     if img.height > 300 or img.width > 300:
+#         output_size = (300, 300)
+#         img.thumbnail(output_size)
+#         img.save(self.image.path)
